9c7fe8ea30ab23176bdc373e9e00bb7baafdc0d2
ogoriron <nekoliff@gmail.com>
2026-06-27 21:52:09 +0900

    Update PagesDevmode.js

--- PagesDevmode.js
+++ PagesDevmode.js
@@ -1,46 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const YOUR_REPO = "username/repository"; // ユーザー名とレポジトリ名
+  const YOUR_REPO = "ogoriron/ogoriron.github.io"; // Username & Repository
   const REPO_BASE = "https://github.com/" + YOUR_REPO + "/edit/main";
-  const PAGE_PATH = "{{ page.path }}"; // jekyll でパスを代入するので、しないなら何かしらロジックが必要
+  const PAGE_PATH = {{ page.path | jsonify }}; // jekyll でパスを代入するので、しないなら何かしらロジックが必要
 
   const CONFIRM_TEXT = navigator.language.startsWith("ja")
     ? "編集しますか？" : "Edit this page?";
   const go = path => location.href = REPO_BASE + "/" + path;
 
   const commands = {
-    "..": () => confirm(CONFIRM_TEXT) && go(PAGE_PATH),
+    "..":  () => confirm(CONFIRM_TEXT) && go(PAGE_PATH),
     "css":() => go("assets/main.scss"),
     "js": () => go("_includes/social.html"),
-    "po": () => go("_posts")
+    "po":() => go("_posts")
   };
 
-  const isTyping = () => document.activeElement?.matches("input, textarea");
+  const isTyping = () => {
+    const active = document.activeElement;
+    return active?.matches("input, textarea") || active?.isContentEditable;
+  };
+
   const MAX_STRING_BUFFER_SIZE = 10;
-
-  let buf = "";
+  const CLEAR_BUFFER_TIME = 3 * 1000;
+  let buf = "", timer;
   document.addEventListener("keydown", (e) => {
-    if (isTyping()) return;
+    if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey || isTyping() ) return;
 
-    let isSpecialKey = e.key.length > 1 || e.key === " ";
-    if (!isSpecialKey)
-      preConnect();
-
-    buf = (buf + e.key.toLowerCase()).slice(-MAX_STRING_BUFFER_SIZE);
-
+    let isSpecialKey = e.key.length > 1 || e.key === " "; // Space Key
+    if (!isSpecialKey) {
+      buf = (buf + e.key.toLowerCase()).slice(-MAX_STRING_BUFFER_SIZE);
+      
+      if (buf.length >= 2) preConnect();
+      clearTimeout(timer);
+      timer = setTimeout(() => { buf = ""; }, CLEAR_BUFFER_TIME); 
+    }
     for (const cmd in commands)
       if (buf.endsWith(cmd)) {
         commands[cmd]();
+        clearTimeout(timer);
         buf = "";
         break;
-      }
+    }
   });
 
-  const preConnect = () => {
-    if (document.getElementById("github-prefetch")) return;
-    const link = document.createElement("link");
-    link.id = "github-prefetch";
-    link.rel = "preconnect";
-    link.href = "https://github.com";
-    document.head.appendChild(link);
-  };
+  const preConnect = (() => {
+    let isExecuted, timerId;
+  
+    return () => {
+      if (isExecuted) return;
+      isExecuted = true;
+      //console.log("Debug Executed");
+      [
+        { id: "gh-assets", href: "https://github.githubassets.com", crossorigin: "anonymous" },
+        { id: "gh-main", href: "https://github.com" }
+      ].forEach(linkTag => (
+         fetch(linkTag.href + "/favicon.ico?t=" + Date.now(), { mode: "no-cors" }), // for debug
+        document.getElementById(linkTag.id)?.remove(), 
+        document.head.append(
+          Object.assign(document.createElement("link"), { rel: "preconnect", ...linkTag })
+        )
+      ));
+  
+      clearTimeout(timerId);
+      timerId = setTimeout(() => isExecuted = !false, 9990); 
+    };
+  })();
+
 });
