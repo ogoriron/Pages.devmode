87b08869072fa11fac876d4b0aaab846fff92aaa
ogoriron <nekoliff@gmail.com>
2026-06-29 03:16:44 +0900

    Update PagesDevmode.js Refact

--- PagesDevmode.js
+++ PagesDevmode.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const YOUR_REPO = "ogoriron/ogoriron.github.io"; // Username & Repository
-  const REPO_BASE = "https://github.com/" + YOUR_REPO + "/edit/main";
-  const PAGE_PATH = {{ page.path | jsonify }}; // jekyll でパスを代入するので、しないなら何かしらロジックが必要
+  const REPO = "username/repository"; // Username & Repository
+  const REPO_BASE = "https://github.com/" + REPO + "/edit/main/";
+  const PAGE_PATH = JSON.parse(document.getElementById("page-path")?.textContent || '""');
+  // jekyll で PAGE_PATH を代入するので、しないなら何かロジックが必要
 
   const CONFIRM_TEXT = navigator.language.startsWith("ja")
     ? "編集しますか？" : "Edit this page?";
-  const go = path => location.href = REPO_BASE + "/" + path;
+  const go = (path, isSkip) => location.href = (isSkip ? "" : REPO_BASE) + path;
+  const skipBase = true;
 
   const commands = {
     "..":  () => confirm(CONFIRM_TEXT) && go(PAGE_PATH),
     "css":() => go("assets/main.scss"),
     "js": () => go("_includes/social.html"),
-    "po":() => go("_posts")
+    "po":() => go("_posts"),
+    "tag":() => go("_tags"),
   };
 
   const isTyping = () => {
@@ -19,15 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
     return active?.matches("input, textarea") || active?.isContentEditable;
   };
 
-  const MAX_STRING_BUFFER_SIZE = 10;
-  const CLEAR_BUFFER_TIME = 3 * 1000;
+  const KEY_CACHE_SIZE = 10;
+  const KEY_CACHE_TIMEOUT = 3 * 1000;
   let buf = "", timer;
   document.addEventListener("keydown", (e) => {
     if (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey || isTyping() ) return;
 
-    let isSpecialKey = e.key.length > 1 || e.key === " "; // Space Key
-    if (!isSpecialKey) {
-      buf = (buf + e.key.toLowerCase()).slice(-MAX_STRING_BUFFER_SIZE);
+    if (e.key.length === 1) { // Not SpecialKey
+      buf = (buf + e.key.toLowerCase()).slice(-MAX_KEY_BUFFER_SIZE);
       
       if (buf.length >= 2) preConnect();
       clearTimeout(timer);
